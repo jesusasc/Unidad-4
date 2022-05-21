1bfe51bb285f4a2cc75fb9b0ea15b0aee2ce4667
jesusasc <jesusasilva958@gmail.com>
2022-05-21 16:21:28 -0500

    Ejercicio 4 y 5

---
+++ Ejercicio4y5/Ejercicio4.php
@@ -0,0 +1,4 @@
+<?php
+    $array = array("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+    var_dump($array);
+?>
---
+++ Ejercicio4y5/Ejercicio5.php
@@ -0,0 +1,8 @@
+<?php       
+$arrayasociativo = array ("Diciembre" => "12", "Noviembre" => "11", "Octubre" => "10", "Septiembre" => "9", "Agosto" => "8", "Julio" => "7", "Junio" => "6", "Mayo" => "5", "Abril" => "4", "Marzo" => "3", "Febrero" => "2", "Enero" => "1" );
+
+//orden ascendente
+asort($arrayasociativo);
+var_export($arrayasociativo);
+?>
+
